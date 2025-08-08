8bec23b8d269edcc3e08ed98d2b8808b22f45031
Md Alam <151665023+mdalam073@users.noreply.github.com>
2025-08-08 14:56:46 +0600

    Update lineage_tissot.mk

--- lineage_tissot.mk
+++ lineage_tissot.mk
@@ -15,9 +15,10 @@ $(call inherit-product, device/xiaomi/tissot/device.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-EVO_BUILD_TYPE := Official
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_USES_PICO_GAPPS := true
+TARGET_EXCLUDES_AUDIOFX := true                   WITH_GMS := false
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true               TARGET_DEFAULT_PIXEL_LAUNCHER := false
+MISTOS_MAINTAINER=Alam
+MIST_BUILD_TYPE := UNOFFICIAL
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := tissot
